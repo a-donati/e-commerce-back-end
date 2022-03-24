92e7be2324e7abbea4e4e7e208e428bb9c22a90e
Angela Donati <angeladonati93@gmail.com>
2022-03-24 11:29:25 -0400

    modify connection.js, fix delete route issue in product-routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -109,14 +109,16 @@ router.put('/:id', (req, res) => {
 // delete one product by its `id` value
 router.delete('/:id', async (req, res) => {
   try {
-    const productData = await Location.destroy({
+    const productData = await Product.destroy({
       where: {
         id: req.params.id
       }
     });
     if (!productData) {
-      res.status(404).json({ message: 'No product found with this id' })
+      res.status(404).json({ message: 'No product found with this id' });
+      return;
     }
+    res.status(200).json(productData);
   } catch (err) {
     res.status(500).json(err);
   }
