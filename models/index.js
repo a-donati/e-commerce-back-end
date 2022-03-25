a286c4c97b51fe3c2bfec95956a2e760229c4400
Angela Donati <angeladonati93@gmail.com>
2022-03-25 15:36:59 -0400

    update model relationships

--- models/index.js
+++ models/index.js
@@ -7,30 +7,25 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
-  // onDelete: 'CASCADE'
 })
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: 'category_id',
-  // onDelete: 'CASCADE',
 })
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: {
-  model: ProductTag,
+  through: "product_tag",
   unique: false,
-  foreignKey: 'product_id'
-  }
-})
+  onDelete: 'CASCADE',
+  foreignKey: "product_id"
+});
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: {
-  model: ProductTag,
+  through: "product_tag",
   unique: false,
   onDelete: 'CASCADE',
   foreignKey: "tag_id"
-  }
-})
+});
 
 module.exports = {
   Product,
